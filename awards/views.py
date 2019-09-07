c1bc9018633c4ada8b00d55b26eda13f69ca8072
Johnonyango <j.yayah7@gmail.com>
2019-09-07 13:44:50 +0300

    create the second view function

--- awards/views.py
+++ awards/views.py
@@ -8,3 +8,10 @@ def welcome(request):
     number = random.randrange(10)
     return render(request, 'home.html', {"projects":projects, "developers": developers, "random":number})
 
+def project(request, project_id):
+    try:
+        project = Project.objects.get(id=project_id)
+        projects = Project.objects.all()
+    except:
+        raise Http404()
+    return render(request, "single_project.html", {"project":project, "projects":projects})
