529854327481959d8a9ec38d59383e34e0f82f0b
Johnonyango <j.yayah7@gmail.com>
2019-09-07 13:44:09 +0300

    create the first view function

--- awards/urls.py
+++ awards/urls.py
@@ -1,7 +1,12 @@
 from django.conf.urls import url
 from . import views
+from django.conf.urls.static import static
+from django.conf import settings
 
-urlpatterns[
+urlpatterns=[
     url(r'^$',views.welcome,name = 'welcome'),
-    
+    url(r'^project/(\d+)/',views.project,name ='project'),
+    url(r'^new/article/$', views.new_project, name='new-project'),
 ]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- awards/views.py
+++ awards/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+'''welcome view to process landing page'''
+def welcome(request):
+    projects = Project.objects.all()
+    developers = Profile.objects.all()
+    number = random.randrange(10)
+    return render(request, 'home.html', {"projects":projects, "developers": developers, "random":number})
+
