fc1892e0e3e4da6723bbc63b6f06eb5aac014e17
Johnonyango <j.yayah7@gmail.com>
2019-09-09 13:06:50 +0300

    testing whether user profile data is saved

--- awards/tests.py
+++ awards/tests.py
@@ -1,3 +1,25 @@
 from django.test import TestCase
+from .models import Profile, Project, Rating
 
-# Create your tests here.
+
+class ProfileTestClass(TestCase):
+  """  
+  Test class that tests profile
+  """
+ def setUp(self):
+    self.prof =Profile(profpic='test.jpg', bio='test bio', contact='hello@hello.com',user=1)
+
+  def test_instance(self):
+      self.assertTrue(isinstance(self.prof, Profile))
+
+  def test_save_method(self):
+      """
+      Function to test that profile is being saved
+      """
+      self.prof.save_profile()
+      profiles = Profile.objects.all()
+      self.assertTrue(len(profiles) > 0)
+
+
+
+    
